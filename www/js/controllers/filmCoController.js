fb2a94088ad3105df8348158c58f95cd5e6a4ecf
Yasmin Green <yasmingreen@Yasmins-MBP.home>
2016-05-27 16:15:47 +0100

    Added functions for barcode api

--- www/js/controllers/filmCoController.js
+++ www/js/controllers/filmCoController.js
@@ -1,7 +1,8 @@
-filmCo.controller("FilmCoController", function($scope, $cordovaBarcodeScanner) {
+filmCo.controller("FilmCoController", function($scope, $cordovaBarcodeScanner, $http) {
   var self = this;
 
   self.barcodeNums = [];
+  self.filmInfo = [];
 
   $scope.scanBarcode = function() {
     $cordovaBarcodeScanner.scan().then(function(imageData) {
@@ -10,4 +11,39 @@ filmCo.controller("FilmCoController", function($scope, $cordovaBarcodeScanner) {
       console.log("An error happened: " + error);
     });
   };
+
+  // $scope.scanBarcode = function() {
+  //   $cordovaBarcodeScanner.scan()
+  //     .then(_apiRequest);
+  // };
+
+  // $scope.scanBarcode = function() {
+  //   $cordovaBarcodeScanner.scan().then(function(imageData) {
+  //     return $http.get("https://api.outpan.com/v2/products/" + imageData.text + "?apikey=NUMBER")
+  //       .then(function(response) {
+  //         self.filmInfo.push(response.data);
+  //       });
+  //   }, function(error) {
+  //     alert("An error happened: " + error);
+  //   });
+  // };
+
+
+  // $scope.apiRequest = function(response) {
+  //   return $http.get("https://api.outpan.com/v2/products/" + self.num + "?apikey=NUMBER")
+  //     .then(_parseResponseFromApi);
+  // };
+
+  // $scope.apiRequest = function() {
+  //   console.log("called");
+  //   return $http.get("https://api.outpan.com/v2/products/8717418407230?apikey=NUMBER")
+  //     .then(function(response) {
+  //       console.log(response);
+  //       self.filmInfo.push(response.data);
+  //     });
+  // };
+
+  // function _parseResponseFromApi(response) {
+  //   return self.filmInfo.push(response.data);
+  // }
 });
